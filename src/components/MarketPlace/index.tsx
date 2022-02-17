b40d0cc8b241b0556a815b98c5a39b06ce82e06b
Josh McLain <joshmclain45@gmail.com>
2022-02-17 16:46:51 -0700

    refactored market and generate func

--- src/components/MarketPlace/index.tsx
+++ src/components/MarketPlace/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { ProductType } from "../../utils/generateMarket";
@@ -7,13 +7,43 @@ const MarketPlace: React.FC = () => {
   const products: undefined | Array<ProductType> = useSelector(
     ({ location: { current } }: RootState) => current.availableProduct
   );
-  
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>): void => {
+    const targetValue: string = event.currentTarget.id;
+    const actionType: string = event.currentTarget.name;
+    if (products) {
+      const targetProduct: ProductType = products.filter(
+        (product: ProductType) => product.productId === targetValue
+      )[0];
+      console.log(actionType, targetProduct);
+    }
+  };
+
   return (
     <div>
       {products &&
         products.map((product: ProductType, index: number) => (
-          <div key={index} style={{ textAlign: "center" }}>
+          <div
+            key={index}
+            style={{ textAlign: "center", marginBottom: "25px" }}
+          >
             <span>{`${product.name} = ${product.quantity}`}</span>
+            <div>
+              <span>price </span>
+              <span>$ {product.price}</span>
+            </div>
+            <div>
+              <span>available </span>
+              <span>{product.quantity}</span>
+            </div>
+            <div>
+              <button name="buy" id={product.productId} onClick={handleClick}>
+                buy
+              </button>
+              <button name="sell" id={product.productId} onClick={handleClick}>
+                sell
+              </button>
+            </div>
           </div>
         ))}
     </div>
--- src/utils/generateMarket.ts
+++ src/utils/generateMarket.ts
@@ -2,72 +2,35 @@ const range = (min: number, max: number): number =>
   Math.round(Math.random() * (max - min + 1) + min);
 
 export type ProductType = {
-    name: string;
-    productId: string;
-    quantity: number;
-    price: number;
-    toBePurchased: number;
-    toBeSold: number;
-}
+  name: string;
+  productId: string;
+  quantity: number;
+  price: number;
+  toBePurchased: number;
+  toBeSold: number;
+};
 
 export const generateMarket = (): Array<ProductType> => {
-  const market = [
-    {
-      name: "Gum Drops",
-      productId: "gumDrops",
-      quantity: range(25, 80),
-      price: range(2, 8),
-      toBePurchased: 0,
-      toBeSold: 0,
-    },
-    {
-      name: "Laffy Taffy",
-      productId: "laffyTaffy",
-      quantity: range(10, 35),
-      price: range(10, 50),
-      toBePurchased: 0,
-      toBeSold: 0,
-    },
-    {
-      name: "Pixie Stix",
-      productId: "pixieStix",
-      quantity: range(5, 50),
+  const productInfo: Array<Array<string>> = [
+    ["Gum Drops", "gumDrops"],
+    ["Pixie Stix", "pixieStix"],
+    ["Gobstoppers", "gobstoppers"],
+    ["Mars Bars", "marsBars"],
+    ["Jelly Beans", "jellyBeans"],
+    ["Laffy Taffy", "laffyTaffy"],
+  ];
+
+  const market: Array<ProductType> = productInfo.map((product: string[]) => {
+    console.log("some logic to generate high and lows");
+    return {
+      name: product[0],
+      productId: product[1],
+      quantity: range(10, 20),
       price: range(5, 25),
       toBePurchased: 0,
       toBeSold: 0,
-    },
-    {
-      name: "Gobstoppers",
-      productId: "gobstoppers",
-      quantity: range(10, 40),
-      price: range(10, 40),
-      toBePurchased: 0,
-      toBeSold: 0,
-    },
-    {
-      name: "Mars Bars",
-      productId: "marsBars",
-      quantity: range(5, 20),
-      price: range(20, 80),
-      toBePurchased: 0,
-      toBeSold: 0,
-    },
-    {
-      name: "Jelly Beans",
-      productId: "jellyBeans",
-      quantity: range(10, 60),
-      price: range(25, 100),
-      toBePurchased: 0,
-      toBeSold: 0,
-    },
-    {
-      name: "Peanut Butter Cups",
-      productId: "peanutButterCups",
-      quantity: range(5, 65),
-      price: range(35, 85),
-      toBePurchased: 0,
-      toBeSold: 0,
-    },
-  ];
+    };
+  });
+
   return market;
 };
